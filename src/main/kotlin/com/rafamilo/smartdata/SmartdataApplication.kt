d5ecafe1f226a35a329facba6ec42f58f3c073f4
rafael.melges <rafael.melges@softplan.com.br>
2021-06-13 15:45:02 -0300

    Add initial structure

---
+++ src/main/kotlin/com/rafamilo/smartdata/SmartdataApplication.kt
@@ -0,0 +1,11 @@
+package com.rafamilo.smartdata
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmartdataApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmartdataApplication>(*args)
+}
---
+++ src/test/kotlin/com/rafamilo/smartdata/SmartdataApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rafamilo.smartdata
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmartdataApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
